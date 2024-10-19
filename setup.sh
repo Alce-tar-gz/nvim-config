581995bea6deb0a32c45d85f0fac5aa33c22da31
Alce-tar-gz <aarni.firmin@gmail.com>
2024-10-19 15:32:57 +0300

    made setup script simpler

--- setup.sh
+++ setup.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
-
-if [ ! -f /usr/bin/nvim ]; then
-  echo "Remember to install Neovim you silly little guy"
-  read -p "Press any key to continue..." -n1 -s
-  exit
-fi
 mv $HOME/.config/nvim $HOME/.config/nvim-backup
 git clone https://github.com/Alce-tar-gz/nvim-config.git $HOME/.config/nvim
 echo "All done!"
