6e0782324ec08fc49c2154968c1fd5e72420b198
Alce-tar-gz <aarni.firmin@gmail.com>
2024-11-03 17:30:08 +0200

    made setup script POSIX-compliant and easy to read

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 mv $HOME/.config/nvim $HOME/.config/nvim-backup
 git clone https://github.com/Alce-tar-gz/nvim-config.git $HOME/.config/nvim
 echo "All done!"
-read -p "Do you want to open Neovim now? [y/n]" REPLY
-if [ $REPLY = "n" ]; then
-  exit
-else
-  nvim
-fi
+echo Do you want to open Neovim now? [y/n]
+read REPLY
+case {$REPLY:l} in
+  n) exit
+    ;;
+  *) exec nvim
+    ;;
+esac
